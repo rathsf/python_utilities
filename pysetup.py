0c14326475731de18381fc16e9b3d4af6c7a09c7
Tatampal <raths.f@gmail.com>
2023-09-16 22:02:09 -0700

    meh

--- pysetup.py
+++ pysetup.py
@@ -1,7 +1,11 @@
 import setuptools
 
+#package_dir = input("Enter package url: ")
 module_name = input("Enter module name: ")
+
 setuptools.setup(
-    name=module_name,
-    py_modules=[module_name],
+    #url         = package_dir,
+    #package_dir = package_dir,
+    name        = module_name, 
+    py_modules  = [module_name],
 )
