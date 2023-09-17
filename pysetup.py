8c12bfe91d080ca37ce9b7d5f6fc475781a050e2
Tatampal <raths.f@gmail.com>
2023-09-16 22:33:47 -0700

    sync

--- pysetup.py
+++ pysetup.py
@@ -1,11 +1,13 @@
 import setuptools
 
-#package_dir = input("Enter package url: ")
+package_dir = input("Enter package url: ")
 module_name = input("Enter module name: ")
+#package_dir = input("Enter package dir: ")
+
 
 setuptools.setup(
-    #url         = package_dir,
-    #package_dir = package_dir,
+    url         = package_dir,
+    #package_dir = {'': package_dir},
     name        = module_name, 
     py_modules  = [module_name],
 )
